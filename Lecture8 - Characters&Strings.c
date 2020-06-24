a21aeb3855dace80053ae9cac16d7aebe6708399
Tugay <33989361+tugaysarici@users.noreply.github.com>
2020-06-24 23:46:09 +0300

    Update Lecture8 - Characters&Strings.c

--- Lecture8 - Characters&Strings.c
+++ Lecture8 - Characters&Strings.c
@@ -1,1 +1,10 @@
+// " " arasına yazılan her şey stringdir.
+// String'in sonu her zaman görünmeyen bir null karakteri olan '\0' ile biter.
+
+char st[] = "JAVA&C";
+char st[] = {'J','A','V','A','&','C', '\0'};
+char *st = "JAVA&C";
+// Yukarıdaki 3 ifadelendirme de aşağıdaki şekilde ekrana basılır.
+printf("%s", st);
+// Ama 3. ifadelendirmede örneğin 3. indise erişmek için *(st+2) kullanılır. Diğerlerinde st[2] kullanılır.
 
