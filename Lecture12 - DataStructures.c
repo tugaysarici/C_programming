a1f7352b43752c07025989186c855ef60a5ab791
Tugay <33989361+tugaysarici@users.noreply.github.com>
2020-06-25 09:55:29 +0300

    Update Lecture12 - DataStructures.c

--- Lecture12 - DataStructures.c
+++ Lecture12 - DataStructures.c
@@ -1,1 +1,29 @@
+struct node {
+  int data;
+  struct node *nextPtr;
+};
 
+newPtr = malloc(sizeof(struct node));
+
+free(newPtr);
+
+// Linked List
+struct node {
+  int data;
+  struct node* next;
+};
+struct node* BuildOneTwoThree() {
+  struct node* head = NULL;
+  struct node* second = NULL;
+  struct node* third = NULL;
+  head = malloc(sizeof(struct node));
+  second = malloc(sizeof(struct node));
+  third = malloc(sizeof(struct node));
+  head->data = 1;
+  head->next = second;
+  second->data = 2;
+  second->next = third;
+  third->data = 3;
+  third->next = NULL;
+  return head;
+}
